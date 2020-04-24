bdfba59b88f90e05a50d281140f311aa2fb17d0c
huangweijie <1539369355@qq.com>
2020-04-24 10:54:53 +0800

    代码规范

--- src/cron/mode/Command.php
+++ src/cron/mode/Command.php
@@ -8,13 +8,15 @@ class Command extends \huangweijie\cron\Mode
         // TODO: Implement handle() method.
        is_string($action) && $action = [$action];
 
-        if (is_array($action)) {
-            foreach ($action as $commandLine) {
-                if (empty($commandLine) || !is_string($commandLine))
-                    continue;
+       if (!is_array($action))
+           return;
 
-                shell_exec("nohup php {$this->think} {$commandLine} >/dev/null 2>&1 &");
-            }
-        }
+       foreach ($action as $commandLine) {
+
+           if (empty($commandLine) || !is_string($commandLine))
+               continue;
+
+           shell_exec("nohup php {$this->think} {$commandLine} >/dev/null 2>&1 &");
+       }
     }
 }
